e34d5c0e10ecf0df902b3cd0ceb6f0f5c417841e
Lalit <30770819+lalitbiswal91@users.noreply.github.com>
2025-12-12 19:57:48 +0100

    Initialize project via Spring Initializer
    Initialize project via Spring Initializer

---
+++ src/test/java/com/lalit/devices/DevicesApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lalit.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class DevicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
